32a38b432d40eb8b37ea1faa62631d906dcf850e
Vitor Henrique <vhenriquebm@gmail.com>
2022-01-05 14:25:17 -0300

    primeira versão do aplicativo

--- caraOucoroa/ViewController.swift
+++ caraOucoroa/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBOutlet weak var moeda: UIImageView!
+    
+    
 
+    
+    @IBAction func botaoJogar(_ sender: Any) {
+        
+        moeda.image = [ #imageLiteral(resourceName: "caraProjeto"), #imageLiteral(resourceName: "coroaProjeto")] [Int.random (in: 0...1)]
+        
+    }
+    
 }
+
+
 
